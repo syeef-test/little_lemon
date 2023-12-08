0135e82429457e54db1b04aa8a4b31d84b4f7456
syeef <syeefislam@yahoo.com>
2023-12-08 20:08:00 +0530

    basic crud function for reservation is done and seperate route for reservation is created

--- backend/index.js
+++ backend/index.js
@@ -2,40 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-import { Reservation } from "./models/reservationModel.js";
+import reservationRoutes from "./routes/reservationRoutes.js";
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to little lemon");
 });
 
-app.post("/reservation", async (req, res) => {
-  try {
-    if (
-      !req.body.username ||
-      !req.body.numberofseats ||
-      !req.body.reservationdate
-    ) {
-      res.status(400).send({ message: "Send all required fields" });
-    }
-    const newReservation = {
-      username: req.body.username,
-      numberofseats: req.body.numberofseats,
-      reservationdate: req.body.reservationdate,
-      reservationdoneon: req.body.reservationdoneon,
-    };
-
-    const reservation = await Reservation.create(newReservation);
-    return res.status(200).send(reservation);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
+app.use("/reservation", reservationRoutes);
 
 async function startServer() {
   try {
---
+++ backend/routes/reservationRoutes.js
@@ -0,0 +1,97 @@
+import express from "express";
+import { Reservation } from "../models/reservationModel.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    if (
+      !req.body.username ||
+      !req.body.numberofseats ||
+      !req.body.reservationdate
+    ) {
+      res.status(400).send({ message: "Send all required fields" });
+    }
+    const newReservation = {
+      username: req.body.username,
+      numberofseats: req.body.numberofseats,
+      reservationdate: req.body.reservationdate,
+      reservationdoneon: req.body.reservationdoneon,
+    };
+
+    const reservation = await Reservation.create(newReservation);
+    return res.status(200).send(reservation);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const reservations = await Reservation.find({});
+    return res.status(200).json({
+      count: reservations.length,
+      data: reservations,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const reservation = await Reservation.findById(id);
+    return res.status(200).json({
+      count: reservation.length,
+      data: reservation,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    if (
+      !req.body.username ||
+      !req.body.numberofseats ||
+      !req.body.reservationdate
+    ) {
+      res.status(400).send({ message: "Send all required fields" });
+    }
+
+    const { id } = req.params;
+    const result = await Reservation.findByIdAndUpdate(id, req.body);
+    if (!result) {
+      return res.status(404).json({ message: "Reservation Details Not Found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Reservation Details Updated Succesfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Reservation.findByIdAndDelete(id);
+    if (!result) {
+      return res.status(404).json({ message: "Reservation Details Not Found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Reservation Details Deleted Succesfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+export default router;
